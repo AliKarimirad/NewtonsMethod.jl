66e67d424450b5bef84ca0c83c5d30046719567c
AliKarimirad <ali.karimirad1369@gmail.com>
2018-11-20 17:15:22 -0800

    uploading form .Julia

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -2,4 +2,46 @@ module NewtonsMethod
 
 greet() = print("Hello World!")
 
+using LinearAlgebra
+function newtonroot(f, f_prime, x_0; tolerance= 1E-7, maxiter= 1000)
+    x_old = x_0
+    error = Inf
+    iter = 1
+    while iter <= maxiter && error > tolerance
+        x_new = x_old - (f(x_old)/f_prime(x_old))
+        error = norm(x_new - x_old)
+        x_old = x_new
+        iter  = iter+1
+    end
+    if error > tolerance
+        return nothing
+    else
+    return (x_old)
+    end
+end
+
+
+using ForwardDiff
+
+D(f) = x -> ForwardDiff.derivative(f, x)
+
+function newtonroot(f, x_0; tolerance= 1E-7, maxiter= 1000)
+    x_old = x_0
+    error = Inf
+    iter = 1
+    f_prime =  D(f)
+    while iter <= maxiter && error > tolerance
+        x_new = x_old - (f(x_old)/f_prime(x_old))
+        error = norm(x_new - x_old)
+        x_old = x_new
+        iter  = iter+1
+    end
+    if error > tolerance
+    return nothing
+    else
+    return (x_old)
+    end
+end
+
+export newtonroot
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,35 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-    # Write your own tests here.
+    # test 1: make sure it is working
+        g(x)=(x-1)^3
+        g_prime(x)=3*(x-1)^2
+        iv = 1.1
+        sol = newtonroot(g, g_prime, iv)
+        @test sol≈ 1.0000001545213348
+    # test 2: make sure that the second dispach works well
+        solu = newtonroot(g, iv)
+    @test solu ≈ 1.0000001545213348
+
+    # test 3: make sure tolerance works
+    solu = newtonroot(g, g_prime, iv, tolerance = 1)
+    solut= newtonroot(g, iv, tolerance =  1)
+    @test !( solu  ≈ 1.0000001545213348)
+    @test !( solut ≈ 1.00000013565)
+
+    # test 4: make sure maxiter works
+    soluti  = newtonroot(g, g_prime, iv,maxiter= 5 )
+    solutio = newtonroot(g, iv, maxiter= 5 )
+    @test soluti  == nothing
+    @test solutio == nothing
+    #test 5: Non-Convergence
+    h(x)=x^2+2
+    h_prime(x)=2x
+    @test newtonroot(h, h_prime, iv)== nothing
+    @test newtonroot(h,  iv)== nothing
+    #test BiFloat
+    iv=BigFloat(3)
+    @test newtonroot(g,g_prime, iv)≈ 1.0000001808754536763325638480
+    @test newtonroot(g, iv)≈ 1.0000001808754536763325638480
+
 end
